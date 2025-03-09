41db607be5ab15f6bc7a40aaec5ace4d5c5d71dd
daniel-kav <dankavatha01@gmail.com>
2025-03-09 07:31:04 +0300

    fetch classes data for announcement type in FormContainer

--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -69,6 +69,12 @@ const FormContainer = async ({ table, type, data, id }: FormContainerProps) => {
         });
         relatedData = { lessons: examLessons };
         break;
+      case "announcement":
+        const announcementClasses = await prisma.class.findMany({
+          select: { id: true, name: true },
+        });
+        relatedData = { classes: announcementClasses };
+        break;
 
       default:
         break;
